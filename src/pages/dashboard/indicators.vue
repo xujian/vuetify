078e41550cf1a20e253c9043fc0c7b5089ef175e
xujian <782135@qq.com>
2018-08-06 15:03:20 +0800

    + sankey chart

--- src/pages/dashboard/indicators.vue
+++ src/pages/dashboard/indicators.vue
@@ -1,11 +1,17 @@
 <template>
-  <v-card color="white">
-    <v-card-text>Indicators</v-card-text>
-  </v-card>
+  <div class="chart-container">
+    <sankey-chart></sankey-chart>
+  </div>
 </template>
 
 <script>
-  export default {
-    
+import LineChart from '@/components/charts/line'
+import SankeyChart from '@/components/charts/sankey'
+
+export default {
+  components: {
+    LineChart,
+    SankeyChart
   }
+}
 </script>
