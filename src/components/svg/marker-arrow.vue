2a0418bd9866309aacf4030a6d2688e243354b8f
xujian <782135@qq.com>
2018-08-17 18:32:57 +0800

    + arrow update style

--- src/components/svg/marker-arrow.vue
+++ src/components/svg/marker-arrow.vue
@@ -1,20 +1,29 @@
 <template>
-    <marker :id="id" viewBox="0 0 10 10" refX="0" refY="5"
-          markerWidth="10" markerHeight="10" markerUnits="userSpaceOnUse"
-          orient="auto">
-        <path d="M10,5 L0,2.5" />
-        <path d="M10,5 L0,5" />
-        <path d="M10,5 L0,2.5" />
-      </marker>
+  <marker :id="id" class="marker-arrow"
+      viewBox="0 0 20 20" refX="0" refY="10"
+      markerWidth="20" markerHeight="20" markerUnits="userSpaceOnUse"
+      orient="auto">
+      <path d="M0,5 V15 L15,10 Z" />
+    </marker>
 </template>
 <script>
 export default {
-    name: 'svg-marker-arrow',
-    props: {
-        id: {
-            type: String,
-            default: 'marker-arrow'
-        }
+  name: 'svg-marker-arrow',
+  props: {
+    id: {
+      type: String,
+      default: 'marker-arrow'
     }
+  }
 }
 </script>
+<style lang="stylus">
+.marker-arrow {
+  path {
+    stroke: none;
+    fill: #009688;
+    fill-opacity: 0.5;
+    stroke-width: 2px;
+  }
+}
+</style>
